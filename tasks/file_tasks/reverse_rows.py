df53f3719486245625152ef8699ba0f22b38ee7a
emys <emys@mail.ru>
2022-08-30 11:23:08 +0300

    revert_text

--- tasks/file_tasks/reverse_rows.py
+++ tasks/file_tasks/reverse_rows.py
@@ -3,3 +3,16 @@
 файл. Создать новый файл, каждая строка которого получается из соответствующей
 строки исходного файла перестановкой слов в обратном порядке.
 """
+
+
+def revert_rows():
+    with open('text.txt') as file:
+        with open('revert_text.txt', 'w') as write_file:
+            for line in file:
+                lst = line.split(' ')
+                revert_row = " ".join(lst[::-1])
+                write_file.write(revert_row)
+    return write_file
+
+
+revert_rows()
